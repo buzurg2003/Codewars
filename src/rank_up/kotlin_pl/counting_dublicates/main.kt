30bd46e03f0a468d81548e04412a729d6a8d4ea8
Programmer_2003 <buzurgcoder2003@gmail.com>
2023-09-04 18:28:47 +0600

    Initial commit

---
+++ src/rank_up/kotlin_pl/counting_dublicates/main.kt
@@ -0,0 +1,7 @@
+package rank_up.kotlin_pl.counting_dublicates
+
+fun main() {
+    val inputString = "aabb"
+    val result = duplicateCount(inputString)
+    println("Count of duplicate characters: $result")
+}
