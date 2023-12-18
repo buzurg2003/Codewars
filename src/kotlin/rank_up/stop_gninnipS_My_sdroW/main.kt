3fbbd724d50b19c602e9c1a2d377fee766aa74c0
Programmer_2003 <buzurgcoder2003@gmail.com>
2023-12-18 23:25:14 +0600

    Deleted some files

--- src/kotlin/rank_up/stop_gninnipS_My_sdroW/main.kt
+++
@@ -1,8 +0,0 @@
-package rank_up.kotlin_pl.stop_gninnipS_My_sdroW
-
-fun main() {
-    // Test cases
-    println(spinWords("Hey fellow warriors"))    // Output: "Hey wollef sroirraw"
-    println(spinWords("This is a test"))          // Output: "This is a test"
-    println(spinWords("This is another test"))    // Output: "This is rehtona test"
-}
